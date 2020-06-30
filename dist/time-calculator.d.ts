3bea3388aa34c3b0e5bd31878774528515add867
Lukas Dattenberger <lukas.datte@gmail.com>
2020-06-30 22:49:56 +0200

    Added dist to git

---
+++ dist/time-calculator.d.ts
@@ -0,0 +1,15 @@
+declare class Time {
+    positive: boolean;
+    hour: number;
+    minute: number;
+    constructor(hour: number, minute: number, positive?: boolean);
+    add(t: Time): void;
+    toString(): string;
+    asMinutes(): number;
+    static cleanTime(hour: number, minute: number): {
+        hour: number;
+        minute: number;
+        positive: boolean;
+    };
+    static fromString(string: string, forcedNegative?: boolean): Time;
+}
